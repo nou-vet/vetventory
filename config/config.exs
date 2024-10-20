1b336109b083a91cd122d0e523812d7d0b364a2b
LexSwed <lexswed@gmail.com>
2024-10-20 15:27:42 +0200

    remove web

--- config/config.exs
+++ config/config.exs
@@ -13,64 +13,9 @@ import Config
 config :hello_ecto,
   ecto_repos: [HelloEcto.Repo]
 
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :hello_ecto, HelloEcto.Mailer, adapter: Swoosh.Adapters.Local
-
 # Configure Mix tasks and generators
 config :hello,
   ecto_repos: [Hello.Repo]
-
-# Configures the mailer
-#
-# By default it uses the "Local" adapter which stores the emails
-# locally. You can see the emails in your browser, at "/dev/mailbox".
-#
-# For production it's recommended to configure a different adapter
-# at the `config/runtime.exs`.
-config :hello, Hello.Mailer, adapter: Swoosh.Adapters.Local
-
-config :hello_web,
-  ecto_repos: [Hello.Repo],
-  generators: [context_app: :hello]
-
-# Configures the endpoint
-config :hello_web, HelloWeb.Endpoint,
-  url: [host: "localhost"],
-  adapter: Bandit.PhoenixAdapter,
-  render_errors: [
-    formats: [html: HelloWeb.ErrorHTML, json: HelloWeb.ErrorJSON],
-    layout: false
-  ],
-  pubsub_server: Hello.PubSub,
-  live_view: [signing_salt: "mzyFt1SR"]
-
-# Configure esbuild (the version is required)
-config :esbuild,
-  version: "0.17.11",
-  hello_web: [
-    args:
-      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
-    cd: Path.expand("../apps/hello_web/assets", __DIR__),
-    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
-  ]
-
-# Configure tailwind (the version is required)
-config :tailwind,
-  version: "3.4.0",
-  hello_web: [
-    args: ~w(
-      --config=tailwind.config.js
-      --input=css/app.css
-      --output=../priv/static/assets/app.css
-    ),
-    cd: Path.expand("../apps/hello_web/assets", __DIR__)
-  ]
 
 # Configures Elixir's Logger
 config :logger, :console,
--- config/test.exs
+++ config/test.exs
@@ -26,13 +26,6 @@ config :hello, Hello.Repo,
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
-# We don't run a server during test. If one is required,
-# you can enable the server option below.
-config :hello_web, HelloWeb.Endpoint,
-  http: [ip: {127, 0, 0, 1}, port: 4002],
-  secret_key_base: "Sd4btg5AFHzj/MOh22aort3CZmS5EsItCWSHr4oCZHIMpD+HtiuJLs9XomAo1DwL",
-  server: false
-
 # Print only warnings and errors during test
 config :logger, level: :warning
 
